a707ebd855fcc2d9ffa4449318df34ec4301c92a
Vinicius Costa <vinicius.dsc95@gmail.com>
2026-04-02 14:30:27 -0300

    refactor: add compound pattern

--- app/photo/[id]/page.tsx
+++ app/photo/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { photoGet } from '@/actions/photo-get'
+import { userGet } from '@/actions/user-get'
 import PhotoContent from '@/components/molecules/photo-content'
 import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
 
 type PhotoIdParams = {
   params: Promise<{ id: string }>
@@ -25,11 +27,20 @@ export const generateMetadata = async ({
 export default async function PhotoIdPage({ params }: PhotoIdParams) {
   const { id } = await params
 
-  const { data } = await photoGet(+id)
+  const { data: photoData } = await photoGet(+id)
+
+  if (!photoData) return notFound()
+
+  const { photo } = photoData
+
+  const user = await userGet()
 
   return (
     <main className="container my-4 grid">
-      <PhotoContent data={data} isSingle />
+      <PhotoContent.Root>
+        <PhotoContent.Image src={photo.src} alt={photo.title} />
+        <PhotoContent.Info photo={photo} user={user} />
+      </PhotoContent.Root>
     </main>
   )
 }
